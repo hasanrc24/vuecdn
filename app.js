a1d67a140cc8a2cf82c4dd483b21ef4397ccdb03
hasanrc24 <hasanrc24@gmail.com>
2023-05-15 16:13:22 +0600

    bs

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ createApp({
   data() {
     return {
       msg: "Vue project initialization.",
+      link: "https://vuejs.org/guide/quick-start.html#using-vue-from-cdn",
+      count: 0,
     };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
   },
 }).mount("#app");
